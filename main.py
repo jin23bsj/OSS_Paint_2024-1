2ecf0c09563e10f1eb2485823f411070acdbf7e4
jin23bsj <bhannalove@naver.com>
2024-06-01 20:46:40 +0900

    [싱크 포크]

--- main.py
+++ main.py
@@ -1,275 +1,84 @@
-"""
-Project : Paint
-paint : 내외부 검은색의 2픽셀 크기의 원을 이용해 그림을 그리는 기능
-clear_paint : 그림판에 있는 그림을 다 지우는 기능
-button_delete : clear_paint의 버튼
+import tkinter as tk
 
-"""
+window = tk.Tk()
+window.title('계산기')
 
-from tkinter import *
-import time #시간 계산을 위한 모듈
-from tkinter.colorchooser import askcolor  # 색상 선택 대화 상자를 가져옴
-import math  # 수학 모듈을 가져옴
+operator = 'none'
+calculation = 0
 
-
-"""
-
-그림판 사용시간을 측정하기 위한 스톱워치 기능을 가진 클래스
-__init__(self, win) : 스톱워치 객체를 초기화하는 메소드
-run(self) : 스톱워치 실행 상태 확인 & 경과 시간을 갱신하는 메소드
-start(self) : 스톱워치를 시작하는 메소드
-stop(self) : 스톱워치를 멈추는 메소드
-reset(self) : 스톱워치를 리셋하는 메소드
-
-"""
-
-class StopWatch():
-    def __init__(self, win): 
-        self.running = False
-        self.start_time = time.time() #시간 시작
-        self.timer_counter = 0 #counter=0
-        self.elapsed_time = 0.0 #지난 시간
-        self.prev_elapsed_time = 0.0
-        self.win = win #정의
-        self.win.title("사용시간 측정") #제목 설정
-       
-        self.timeText = Label(win, height = 5, text="0.000", font=("Arial 30 bold")) #글씨 및 폰트
-        self.timeText.pack(side = TOP) #위치
-        self.startButton = Button(win, width=10, text="Start", bg="sky blue", command=self.start) 
-        self.startButton.pack(side = LEFT) 
-        self.stopButton = Button(win, width=10, text="Stop", bg="red", command=self.stop) 
-        self.stopButton.pack(side = LEFT)
-        self.resetButton = Button(win, width=10, text="Reset", bg="light pink", command=self.reset) 
-        self.resetButton.pack(side = LEFT) 
-
-    def run(self): 
-        if self.running == True: #if
-            self.timer_counter += 1 #1씩 더하기
-            cur_time = time.time() #시간
-            time_diff = cur_time - self.start_time
-            self.elapsed_time = time_diff + self.prev_elapsed_time #경과시간 계산
-            self.timeText.configure(text="{:.3f}".format(self.elapsed_time)) #경과시간 출력
-        self.win.after(10, self.run) # sleep 10 ms
-
-
-    def start(self): 
-        if (self.running != True):
-            self.running = True
-            self.start_time = time.time() #시간 재기 
-            self.prev_elapsed_time = self.elapsed_time
-
-    def stop(self): 
-        self.running = False 
-        self.prev_elapsed_time = self.elapsed_time 
-
-    def reset(self): 
-        self.running = False
-        self.timer_counter = 0 #0으로 초기화
-        self.elapsed_time = 0.0 #경과시간 0
-        self.prev_elapsed_time = 0.0 
-        self.timeText.configure(text="{:.3f}".format(self.elapsed_time)) #0.00st로 나타남
-
-
-
-
-
-# 초기 설정 값들
-selected_shape = "oval"  # 기본 도형은 타원형으로 설정
-current_color = "black"  # 기본 색상은 검은색으로 설정
-eraser_mode = False  # 기본적으로 지우개 모드는 비활성화
-spacing = 10  # 도형 사이의 최소 간격을 10으로 설정
-last_x, last_y = None, None  # 마지막 마우스 위치를 저장할 변수 초기화
-
-# 마우스 움직임에 따라 도형을 그리는 함수
-def set_paint_mode_normal():
-    canvas.bind("<B1-Motion>", paint)
-
-def set_paint_mode_pressure():
-    canvas.bind("<Button-1>", start_paint_pressure)  # 마우스 클릭시작시
-    canvas.bind("<B1-Motion>", paint_pressure) #마우스를 클릭중일시 -> 그림을 그리고 있을시
-
-def start_paint_pressure(event):
-    global start_time
-    start_time = time.time() #마우스를 클릭한 시간을 변수에 저장
-
-def paint_pressure(event):
-    global start_time
-    elapsed_time = time.time() - start_time  # 마우스를 클릭한 시간부터 지금까지의 시간을 계산
-    radius = min(max(int(elapsed_time * 5), 1), 5)  # 굵가는 마우스 클릭 시간에 비례하여 최대 5까지 증가
-    x1, y1 = ( event.x - radius ), ( event.y - radius )
-    x2, y2 = ( event.x + radius ), ( event.y + radius )
-    canvas.create_oval(x1, y1, x2, y2, fill=brush_color, outline=brush_color)
-
-def paint_start(event):
-    global x1, y1
-    x1, y1 = (event.x - brush_size), (event.y - brush_size)
-
-def paint(event):
-    global x1, y1
-    x2, y2 = event.x, event.y
-    canvas.create_line(x1, y1, x2, y2, fill=brush_color, width=2)
-    x1, y1 = x2, y2
-
-"""
-dotted_paint: 점선 브러쉬 함수
-이벤트가 발생한 위치에 검은색 원을 일정한 간격으로 그린다.
-매개변수: event - 마우스 이벤트 객체로, 마우스의 현재 좌표를 포함
-"""
-def dotted_paint(event): # 점선 브러쉬 함수
-    global last_x, last_y
-    spacing = 10  # 점 사이의 간격을 설정
-    if last_x is not None and last_y is not None:
-        dx = event.x - last_x
-        dy = event.y - last_y
-        distance = (dx ** 2 + dy ** 2) ** 0.5
-        if distance >= spacing:
-            canvas.create_oval(event.x-1, event.y-1, event.x+1, event.y+1, fill="black", outline="black")
-            last_x, last_y = event.x, event.y
+def button_click(key=0):
+    global num1, num2
+    if operator == 'none':
+        if print_value.get() == '0':
+            print_value.set(str(key))
+            num1 = key
+        else:
+            num1 *= 10
+            num1 += key
+            print_value.set(str(num1))
     else:
-        last_x, last_y = event.x, event.y
+        if print_value.get() == '0' or num2 == 0:
+            print_value.set(str(key))
+            num2 = key
+        else:
+            num2 *= 10
+            num2 += key
+            print_value.set(str(num2))
 
-"""
-set_brush_mode: 브러쉬 모드를 변경하는 함수
-실선 브러쉬와 점선 브러쉬로 전환한다.
-매개변수: mode - 브러쉬 모드를 나타내는 문자열 ("solid" 또는 "dotted")
-"""
-def set_brush_mode(mode): # 브러쉬 모드를 변경하는 함수
-    global brush_mode
-    brush_mode = mode
-    if brush_mode == "solid": # 브러쉬 모드가 solid면 
-        canvas.bind("<B1-Motion>", paint) # 실선(기본) 브러쉬로 변경
-    elif brush_mode == "dotted": # 브러쉬 모드가 dotted면
-        canvas.bind("<B1-Motion>", dotted_paint) # 점선 브러쉬로 변경
+def operator_click(op):
+    global operator
+    operator = op
 
-# 슬라이더를 통해 펜 굵기를 변경하는 함수
-def change_brush_size(new_size):
-    global brush_size
-    brush_size = int(new_size)
+def calculate():
+    global num1, num2, operator
+    if operator == '+':
+        calculation = num1 + num2
+    elif operator == '-':
+        calculation = num1 - num2
+    else:
+        calculation = 0
 
-#all clear 기능 추가
-def clear_paint():
-    canvas.delete("all")
-    global last_x, last_y
-    last_x, last_y = None, None # 마지막 좌표 초기화
+    print_value.set(str(calculation))
+    num1 = 0
+    num2 = 0
+    operator = 'none'
 
-def add_text(event):# 텍스트 박스의 내용을 가져와서 클릭한 위치에 텍스트를 추가합니다.
+def initialize():
+    global num1, num2, operator
+    num1 = 0
+    num2 = 0
+    operator = 'none'
+    print_value.set('0')
 
-    text = text_box.get()
-    canvas.create_text(event.x, event.y, text=text, fill="black", font=('Arial', 12))
-   
+# 출입력 창
+num1, num2 = 0, 0
+print_value = tk.StringVar()
+print_value.set(0)
+display = tk.Entry(window, width=25, textvariable=print_value, justify='right', font=('Arial', 24))
+display.grid(columnspan=4, row=0, ipadx=8, ipady=20, sticky='nsew')
 
-def toggle_fullscreen(event):
-    window.state = not window.state
-    window.attributes("-fullscreen", window.state)
+# 버튼 크기 조절 및 배치
+buttons = [
+    ('1', 1, 1), ('2', 1, 2), ('3', 1, 3), ('C', 1, 4),
+    ('4', 2, 1), ('5', 2, 2), ('6', 2, 3), ('+', 2, 4),
+    ('7', 3, 1), ('8', 3, 2), ('9', 3, 3), ('-', 3, 4),
+    ('0', 4, 1), ('=', 4, 4)
+]
 
-# 좌우 반전 기능 추가
-def flip_horizontal():
-    objects = canvas.find_all()
-    canvas.update()
-    canvas_width = canvas.winfo_width()
-    for obj in objects:
-        coords = canvas.coords(obj)
-        for i in range(len(coords)):
-            if i % 2 == 0:  # x 좌표를 반전시킵니다.
-                coords[i] = canvas_width - coords[i]
-        canvas.coords(obj, *coords)
+for (text, row, col) in buttons:
+    if text == 'C':
+        button = tk.Button(window, text=text, command=initialize)
+    elif text in '+-':
+        button = tk.Button(window, text=text, command=lambda op=text: operator_click(op))
+    elif text == '=':
+        button = tk.Button(window, text=text, command=calculate)
+    else:
+        button = tk.Button(window, text=text, command=lambda key=int(text): button_click(key))
+    button.grid(row=row, column=col, sticky='nsew', padx=1, pady=1)
 
-def erase(event):
-    bg_color = canvas.cget("bg")
-    # 그림을 지우기 편하도록 paint의 픽셀보다 더욱 크게 설정
-    x1, y1 = ( event.x-3 ), ( event.y-3 )
-    x2, y2 = ( event.x+3 ), ( event.y+3 )
-    canvas.create_oval(x1, y1, x2, y2, fill=bg_color, outline=bg_color)
-
-def change_bg_color():
-    bg_color = askcolor()
-    canvas.config(bg=bg_color[1])
-
-def change_brush_color():
-    global brush_color
-    brush_color = askcolor()[1]
-
-# 새 창 열기 생성
-def create_new_window():
-    new_window = Tk()  #새로운 Tk 인스턴스 생성
-    new_canvas = Canvas(new_window) # 새로운 창에 캔버스 추가
-    new_canvas.pack() #캔버스가 새로운 창에 배치
-    new_window.mainloop()
-
-win = Tk()
-stop_watch = StopWatch(win) #스톱워치 클래스에서 인스턴스를 생성
-stop_watch.run()
-
-
-
-window = Tk()
-#Tk 객체를 생성하여 주 윈도우를 만들기
-window.title("그림판")
-
-brush_size = 1  # 초기 브러시 크기
-canvas = Canvas(window, bg="white")
-#Canvas 위젯을 생성하여 주 윈도우에 추가
-window.geometry("640x400+200+200")
-#윈도우이름.geometry("너비x높이+x좌표+y좌표")를 이용하여
-#윈도우 창의 너비와 높이, 초기 화면 위치의 x좌표와 y좌표를 설정
-window.resizable(True,True)
-#윈도우이름.resizeable(상하, 좌우)을 이용하여
-#윈도우 창의 창 크기 조절 가능 여부를 설정
-canvas.pack(fill="both",expand=True)
-#캔버스를 창 너비에 맞춰 동적으로 크기 조절
-
-last_x, last_y = None, None # 마지막 좌표 초기화
-brush_mode = "solid"  # 기본 브러쉬 모드를 실선으로 설정
-canvas.bind("<Button-1>", paint_start)
-canvas.bind("<B1-Motion>", paint)
-# 캔버스에 마우스 왼쪽 버튼을 누르고 움직일 때마다 paint 함수를 호출하도록 바인딩
-
-button_frame = Frame(window)
-button_frame.pack(fill=X)
-
-button_clear = Button(button_frame, text="All Clear", command=clear_paint)
-button_clear.pack(side=LEFT)
-
-# 펜 굵기를 조절할 수 있는 슬라이더 추가
-brush_size_slider = Scale(button_frame, from_=1, to=20, orient=HORIZONTAL, label="Brush Size", command=change_brush_size)
-brush_size_slider.set(brush_size)  # 슬라이더 초기값 설정
-brush_size_slider.pack(side=LEFT)
-
-button_solid = Button(window, text="Solid Brush", command=lambda: set_brush_mode("solid")) # 버튼을 누르면 실선 모드로 바꾼다
-button_solid.pack() # 실선 브러쉬 버튼을 윈도우에 배치
-
-button_dotted = Button(window, text="Dotted Brush", command=lambda: set_brush_mode("dotted")) # 버튼을 누르면 점선 모드로 바꾼다
-button_dotted.pack() # 점선 브러쉬 버튼을 윈도우에 배치
-
-button_paint = Button(window, text="normal", command=set_paint_mode_normal) #기본 그리기 모드로 전환하는 기능
-button_paint.pack(side=RIGHT)
-
-button_paint = Button(window, text="pressure", command=set_paint_mode_pressure) #감압 브러시 그리기 모드로 전환하는 기능
-button_paint.pack(side=RIGHT)
-
-text_box = Entry(window) #텍스트를 입력할 공간을 생성합니다.
-text_box.pack(side=LEFT)
-canvas.bind("<Button-3>", add_text) #입력한 텍스트를 오른쪽 클릭으로 텍스트를 찍어냅니다.
-window.bind("<F11>", toggle_fullscreen)
-
-button_new_window = Button(window, text="새 창 열기", command=create_new_window) #"새 창 열기"라는 버튼 생성 command: 버튼 클릭 시 create_new_window: 새로운 창을 만듦 
-button_new_window.pack(side=LEFT) # "새 창 열기"버튼을 윈도우에 배치
-
-button_flip = Button(window, text="Flip Horizontal", command=flip_horizontal)
-button_flip.pack(side=LEFT)
-
-canvas.bind("<B3-Motion>", erase)
-
-brush_color = "black"
-
-button_bg_color = Button(window, text="Change Background Color", command=change_bg_color)
-button_bg_color.pack(side=LEFT)
-
-button_brush_color = Button(window, text="Change Brush Color", command=change_brush_color)
-button_brush_color.pack(side=LEFT)
-
-set_paint_mode_normal() # 프로그램 시작 시 기본 그리기 모드 설정
+# grid 행 및 열 크기 조절
+for i in range(1, 5):
+    window.grid_rowconfigure(i, weight=1)
+    window.grid_columnconfigure(i, weight=1)
 
 window.mainloop()
-
-
